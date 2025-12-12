c4084d9b0db5e510e369ce4b33c08f5d0e471978
Djidjoho Thierry Hodehou <thierry.hodehou@gmail.com>
2025-12-12 10:23:00 +0100

    Remove all .gitkeep files

---
+++ scripts/Plot-QTL-colocalization.R
@@ -0,0 +1,39 @@
+source("functions/QTL_colocalization.R")
+
+qtl_data <- read.csv("data/sheet.csv")
+qtl_data$Trait <- as.factor(qtl_data$Trait)
+qtl_data$Chr_string <- factor(qtl_data$Chr_string,
+                              levels = paste0("Chr", sprintf("%02d", 1:10)))
+qtl_data$Start_QTL_v3 <- as.numeric(qtl_data$Start_QTL_v3)
+qtl_data$End_QTL_v3   <- as.numeric(qtl_data$End_QTL_v3)
+
+mycol <- c("FLA"="royalblue","Mold"="firebrick","YLD"="gold")
+
+ma_genes <- data.frame(
+  Gene = c("Ma1","Ma2","Ma3","Ma5","Ma6"),
+  Chr  = c("Chr06","Chr02","Chr01","Chr01","Chr06"),
+  Pos  = c(40304883, 67882606, 68034103, 6748036, 697459)
+)
+
+chr_levels <- levels(qtl_data$Chr_string)
+
+jpeg("outputs/QTL_colocalization_genes_function.jpg", width=1600, height=1200, res=150)
+
+segplot(
+  reorder(factor(QTL_Id), Chr_string) ~ Start_QTL_v3 + End_QTL_v3 | Chr_string,
+  data = qtl_data,
+  groups = Trait,
+  level = qtl_data$Trait,
+  layout = c(10,1),
+  draw.bands = FALSE,
+  lty = 1, lwd = 6,
+  col.regions = mycol,
+  ylab = "",
+  xlab = "",
+  colorkey = NULL,
+  
+  scales = list(x=list(draw=FALSE)),
+  panel = add_ma_genes(ma_genes, chr_levels)  # <-- ici on ajoute les gènes
+)
+
+dev.off()
